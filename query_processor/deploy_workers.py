6f9b4119f2eff51d0c91ffab9ed13dd88fadb84b
Satria Kautsar <satria@pop-os.localdomain>
2022-09-27 14:52:57 -0400

    fix

--- query_processor/deploy_workers.py
+++ query_processor/deploy_workers.py
@@ -65,10 +65,9 @@ def deploy_jobs(pending, jobs_db, npdc_db, instance_folder, num_threads, ram_siz
             )
             blast_output_path = path.join(temp_dir, "output.txt")
             try:
-                cmd = "{}".format(
-                    "srun -c {} -n 1 --mem={}G -t 1000 ".format(num_threads, ram_size_gb) if use_srun else ""
-                ) +
-                "diamond blastp -d {} -q {} -e 1e-10 -o {} -f 6 {} --ignore-warnings --query-cover 80 --id 40 -k 999999 -p {} -b{:.1f} -c1".format(
+                cmd = 
+                "{}diamond blastp -d {} -q {} -e 1e-10 -o {} -f 6 {} --ignore-warnings --query-cover 80 --id 40 -k 999999 -p {} -b{:.1f} -c1".format(
+                    "srun -c {} -n 1 --mem={}G -t 1000 ".format(num_threads, ram_size_gb) if use_srun else "",
                     diamond_blast_db_path,
                     fasta_input_path,
                     blast_output_path,
