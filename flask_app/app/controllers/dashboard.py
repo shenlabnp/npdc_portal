13cd841b7a9752d6fd3297310f8d988ea78711fc
Satria Kautsar <satria@pop-os.localdomain>
2022-09-29 10:44:13 -0400

    fix

--- flask_app/app/controllers/dashboard.py
+++ flask_app/app/controllers/dashboard.py
@@ -30,17 +30,17 @@ def page_dashboard():
 
     num_registered_user = pd.read_sql_query((
         "select count(id) from users"
-    ),  sqlite3.connect(conf["user_db_path"])).iloc[0, 0]
+    ),  sqlite3.connect(conf["user_db_path"], timeout=60)).iloc[0, 0]
 
 
     num_login_today = pd.read_sql_query((
         "select count(id) from users where last_login >= ?"
-    ),  sqlite3.connect(conf["user_db_path"]), params=(datetime.now().strftime("%Y-%m-%d 00:00:00"),)).iloc[0, 0]
+    ),  sqlite3.connect(conf["user_db_path"], timeout=60), params=(datetime.now().strftime("%Y-%m-%d 00:00:00"),)).iloc[0, 0]
 
     users_countries = pd.read_sql_query((
         "select countries.name from users left join user_details on users.id=user_details.user_id"
         " left join countries on countries.code=user_details.country"
-    ),  sqlite3.connect(conf["user_db_path"])).iloc[:, 0]
+    ),  sqlite3.connect(conf["user_db_path"], timeout=60)).iloc[:, 0]
     users_countries = list(sorted(users_countries.value_counts().to_dict().items(), key=lambda x: x[1], reverse=True))
 
     users_jobs_academia = pd.read_sql_query((
@@ -52,7 +52,7 @@ def page_dashboard():
         " left join job_titles on job_titles.id=user_details.job_title"
         " where user_details.is_academics=1"
         " group by name"
-    ),  sqlite3.connect(conf["user_db_path"])).values.tolist()
+    ),  sqlite3.connect(conf["user_db_path"], timeout=60)).values.tolist()
 
     users_jobs_nonacademia = pd.read_sql_query((
         "select 'n/a' as name, count(users.id) as count_users,"
@@ -62,17 +62,17 @@ def page_dashboard():
         " from users left join user_details on users.id=user_details.user_id"
         " where user_details.is_academics=0"
         " group by name"
-    ),  sqlite3.connect(conf["user_db_path"])).values.tolist()
+    ),  sqlite3.connect(conf["user_db_path"], timeout=60)).values.tolist()
 
     blast_queries = pd.read_sql_query((
         "select status_enum.name, count(jobs.id) from jobs left join status_enum on jobs.status=status_enum.code"
         " group by status_enum.code"
-    ),  sqlite3.connect(conf["query_db_path"])).values.tolist()
+    ),  sqlite3.connect(conf["query_db_path"], timeout=60)).values.tolist()
 
     user_downloads = pd.read_sql_query((
         "select npdc_id, sum(count_download_genome), sum(count_download_bgc) from user_downloads"
         " group by npdc_id"
-    ),  sqlite3.connect(conf["user_db_path"])).values.tolist()
+    ),  sqlite3.connect(conf["user_db_path"], timeout=60)).values.tolist()
 
     # render view
     return render_template(
